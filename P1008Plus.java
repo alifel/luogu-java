c7d3a31e601bd98ad3c6cdd25bb0c71ef9e8b65c
llgn <main@lvluoguniang.com>
2021-06-06 01:58:25 +0800

    P1008Plus

---
+++ P1008Plus.java
@@ -0,0 +1,8 @@
+public class P1008Plus {
+    public static void main(String args[]){
+        System.out.println("192 384 576");
+        System.out.println("219 438 657");
+        System.out.println("273 546 819");
+        System.out.println("327 654 981");
+    }
+}
