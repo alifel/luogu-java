91b47ce8881e6f72bb4a8361022cb8a4109a909f
llgn <main@lvluoguniang.com>
2021-06-06 02:13:50 +0800

    p1008Top重新提交

--- P1008Top.java
+++ P1008Top.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class P1008Top {
+public class P1008Top {
+    public static void main(String args[]){
+        for(int a = 123 ; a < 330 ; a ++){
+            int b = a * 2;
+            int c = a * 3;
+            if(ok(a, b, c)){
+                System.out.println(a + " " + b + " " + c);
+            }
+        }
+    }
+    public static boolean ok(int a, int b, int c){
+        String v = String.valueOf(a) + String.valueOf(b) + String.valueOf(c);
+        if(v.contains("1") && v.contains("2") && v.contains("3") && v.contains("4") && v.contains("5") && v.contains("6") && v.contains("7") && v.contains("8") && v.contains("9"))
+            return true;
+        return false;
+    }
 }
